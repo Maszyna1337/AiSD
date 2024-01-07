301e79d5f66d60e2db29b4283570dc02af30a72d
Jakub Sobczyński <115147227+Maszyna1337@users.noreply.github.com>
2024-01-07 11:20:27 +0100

    update binary

--- aisd_drzewabinarne/aisd_drzewabinarne/Form1.cs
+++ aisd_drzewabinarne/aisd_drzewabinarne/Form1.cs
@@ -39,7 +39,20 @@ namespace aisd_drzewabinarne
                     this.right = child;
                 }
             }
-            
+            public int getNumberOfChildren()
+            {
+                int number = 0;
+                if (this.left != null)
+                {
+                    number++;
+                }
+                if(this.right != null)
+                {
+                    number++;
+                }
+                return number;
+            }
+
         }
         public class binaryTree
         {
@@ -80,6 +93,99 @@ namespace aisd_drzewabinarne
                     w = w.right;
                 }
                 return w.value;
+            }
+            public wezel deleteWhen0(wezel w)
+            {
+                if(w.parent == null)
+                {
+                    this.root = null;
+                    return w;
+                }
+                if(w.parent.left == w)
+                {
+                    w.parent.left = null;
+                    return w;
+                }
+                else
+                {
+                    w.parent.right = null;
+                    return w;
+                }
+            }
+            public wezel deleteWhen1(wezel w)
+            {
+                wezel child = null;
+                if (w.left != null)
+                {
+                    child = w.left;
+                    w.left = null;
+                }
+                else
+                {
+                    child = w.right;
+                    w.right = null;
+                }
+                if(w.parent == null)
+                {
+                    this.root = child;
+                }
+                else
+                {
+                    if(w.parent.left == w)
+                    {
+                        w.parent.left = child;
+                    }
+                    else
+                    {
+                        w.parent.right = child;
+                    }
+                }
+                child.parent = w.parent;
+                w.parent = null;
+                return w;
+            }
+            public wezel deleteWhen2(wezel w)
+            {
+                wezel childL = w.left;
+                wezel childR = w.right;
+                w.left = null;
+                w.right = null;
+                if (w.parent == null)
+                {
+                    this.root = childR;
+                }
+                else
+                {
+                    if (w.parent.left == w)
+                    {
+                        w.parent.left = childL;
+                    }
+                    else
+                    {
+                        w.parent.right = childR;
+                    }
+                }
+                childL.parent = w.parent;
+                childR.parent = w.parent;
+                w.parent = null;
+                return w;
+            }
+
+            public wezel delete(wezel w)
+            {
+                switch (w.getNumberOfChildren())
+                {
+                    case 0:
+                        w = this.deleteWhen0(w);
+                        break;
+                    case 1:
+                        w = this.deleteWhen1(w);
+                        break;
+                    case 2:
+                        w = this.deleteWhen2(w);
+                        break;
+                }
+                return w;
             }
         }
         
